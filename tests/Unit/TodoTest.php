119433fb9add22f98c6fc06033545c80d7b537ca
jonathanmusila <jonathanmusila6@gmail.com>
2021-03-11 17:41:41 +0300

    created tests

--- tests/Unit/TodoTest.php
+++ tests/Unit/TodoTest.php
@@ -1,5 +1,20 @@
 <?php
 
-test('todo', function () {
-    assertTrue(true);
+use App\Models\Todo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+it('does not create a todo without a name field', function () {
+    $response = $this->postJson('/api/todos', []);
+    $response->assertStatus(422);
+});
+
+it('can create a todo', function(){
+    $attributes = Todo::factory()->raw();
+    $response = $this->postJson('/api/todos', $attributes);
+    $response->assertStatus(201)->assertJson([
+        'msg' => 'Todo created successfully'
+    ]);
+    $this->assertDatabaseHas('todos', $attributes);
 });
