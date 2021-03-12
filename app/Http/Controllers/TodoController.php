1ffd3e78c576caa438a966a6c023d0594290e30d
jonathanmusila <jonathanmusila6@gmail.com>
2021-03-12 11:41:37 +0300

    added create method

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:4'
+        ];
+    }
 }
