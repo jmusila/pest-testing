5fdbf9a0c426fa5a973a63d59b3eb798667e415c
jonathanmusila <jonathanmusila6@gmail.com>
2021-03-12 11:48:15 +0300

    added create method

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,19 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function create(Request $request)
     {
+        $request->validate($this->rules());
 
+        $todo = Todo::create($request->get('name'));
+
+        $data = [
+            'msg' => 'Todo created successfully',
+            'todo' => $this->mapTodoResponse($todo)
+        ];
+
+        return response()->json($data, 200);
     }
 
     public function rules()
     {
         return [
             'name' => 'required|string|min:4'
+        ];
+    }
+
+    public function mapTodoResponse($todo)
+    {
+        return [
+            'id' => $todo->id,
+            'name' => $todo->name,
+            'competed' => $todo->completed
         ];
     }
 }
