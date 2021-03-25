7a2fb8a6a88bba1e70c8174c165754f6ee22b316
jonathanmusila <jonathanmusila6@gmail.com>
2021-03-25 12:25:47 +0300

    added get user test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -55,3 +55,20 @@ it('cannot create a user with duplicate email', function () use($base_url){
     $response2 = $this->postJson("{$base_url}", $attributes);
     $response2->assertStatus(422);
 });
+
+it('can fetch a single users', function() use($base_url){
+    $this->withoutExeptionHandling();
+    $user = User::factory()->create();
+    $response = $this->getJson("{$base_url}/{$user->id}");
+
+    $data = [
+        'msg' => 'Todo retrived successfully',
+        'data' => [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email
+        ]
+    ];
+
+    $response->assertStatus(200)->assertJson($data);
+});
